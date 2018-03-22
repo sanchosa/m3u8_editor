45dc0d71761093d18b43e156c8dddd1402ec5d44
unknown <sanchosa@mail.ru>
2018-03-22 14:42:40 +0600

    Converted readFile to promise function.

--- src/utils/read-file.js
+++ src/utils/read-file.js
@@ -1,21 +1,31 @@
 import {notification} from 'antd'
 
-export default fileObj => {
-	if (!fileObj.file) return null
+const readTextFilePromise = fileObj => {
+	return new Promise((resolve, reject) => {
+		const reader = new FileReader()
+		reader.readAsText(fileObj.file)
+		reader.onload = () => resolve(reader.result)
+		reader.onerror = error => reject(error)
+	})
+}
 
-	const reader = new FileReader()
-	reader.readAsText(fileObj.file)
-	reader.onload = () => reader.result
-	reader.onerror = error => {
-		const config = {
-			description: `File Upload error`,
-			duration: null,
-			message: `${error}`,
-			placement: `topLeft`
-		}
-
-		notification[`error`](config)
-
-		return null
+const errorHandler = error => {
+	const config = {
+		description: `File Upload error`,
+		duration: null,
+		message: `${error}`,
+		placement: `topLeft`
 	}
+
+	notification[`error`](config)
+
+	return ``
+}
+
+export default fileObj => {
+	if (!`file` in fileObj) return ``
+
+	return readTextFilePromise(fileObj)
+		.then(result => result)
+		.catch(errorHandler)
 }
