d73b7a6e0e8bdcdf49a5f3e77a978dcf19330039
unknown <sanchosa@mail.ru>
2018-04-01 16:02:48 +0600

    Changed error trapper display content.

--- src/components/ErrorTrapper/index.jsx
+++ src/components/ErrorTrapper/index.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import {notification} from 'antd'
 
 export default class ErrorTrapper extends React.PureComponent {
 	constructor(props) {
@@ -22,26 +23,19 @@ export default class ErrorTrapper extends React.PureComponent {
 	displayError(hasError) {
 		if (!hasError) return null
 		const {code, message, stack} = this.props.error
-		const lines = stack
-			.split(/\n/)
-			.slice(1)
-			.map(line => <li>{line}<br/></li>)
-		return (
-			<div>
-				<h1>🚨 Уии ууу уиииуу, ошибка! 🚨</h1>
-				<p>✔ Код ошибки: {code}</p>
-				<p>✔ {message}</p>
-				<ul>{lines}</ul>
-				<button onClick={() => this.clearError()}>Закрыть</button>
-			</div>
-		)
+		const config = {
+			description: `${stack || ``}`,
+			duration: null,
+			message: `${code || ``} - ${message}`,
+			placement: `topLeft`,
+			onClose: () => this.clearError()
+		}
+
+		notification[`error`](config)
+		return null
 	}
 	render() {
 		const hasError = this.props.error.code > 0
-		return (
-			<div>
-				{this.displayError(hasError)}
-			</div>
-		)
+		return this.displayError(hasError)
 	}
 }
