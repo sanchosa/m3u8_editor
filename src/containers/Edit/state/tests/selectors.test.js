352823998ff280458d9f16eb6d9803d8eba859e9
sanchosa <sanchosa@mail.ru>
2018-05-16 23:50:35 +0600

    Added makeSelectTransferData selector test.

--- src/containers/Edit/state/tests/selectors.test.js
+++ src/containers/Edit/state/tests/selectors.test.js
@@ -1,15 +1,27 @@
 import {Map} from 'immutable'
 import {initialState} from '../constants'
-import {makeSelectEditParam} from '../selectors'
+import {initialState as listEditor} from 'containers/ListEditor/state/constants'
+import {makeSelectEditParam, makeSelectTransferData} from '../selectors'
 import {testValues} from 'common/constants'
 
-const mockedState = Map({Edit: initialState})
-	.setIn([`edit`, `leftGroup`], testValues.string)
+const mockedState = Map({
+	edit: initialState,
+	listEditor
+})
 
 describe(`Edit state selectors`, () => {
 	it(`Should select param from Edit data`, () => {
-		const expectedResult = mockedState.getIn([`edit`, `leftGroup`])
+		const State = mockedState.setIn([`edit`, `leftGroup`], testValues.string)
+		const expectedResult = State.getIn([`edit`, `leftGroup`])
 		const makeSelect = makeSelectEditParam(`leftGroup`)
+		expect(makeSelect(State)).toEqual(expectedResult)
+	})
+	it(`Should select default data for Transfer component`, () => {
+		let expectedResult = Map({})
+		const makeSelect = makeSelectTransferData()
 		expect(makeSelect(mockedState)).toEqual(expectedResult)
+		expectedResult = Map({dataSource: [], targetKeys: []})
+		const State = mockedState.setIn([`edit`, `leftGroup`], testValues.string)
+		expect(makeSelect(State)).toEqual(expectedResult)
 	})
 })
