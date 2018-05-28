d8a2af8c9ca9ab0a271cc9bc871412807d58ece3
sanchosa <sanchosa@mail.ru>
2018-05-29 00:51:44 +0600

    Added header logo.

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 import styled from 'styled-components'
 import {Layout, Row, Col} from 'antd'
 import LanguageSelector from 'containers/LanguageSelector'
-import OpenFile from 'containers/OpenFile'
 import Menu from 'containers/Menu'
+import Logo from 'assets/images/Logo.png'
 
 const {Header} = Layout
 
@@ -11,12 +11,15 @@ const StyledHeader = styled(Header)`
 	box-shadow: 0px 3px 8px #f0f1f2;
 	background: #fff;
 `
+const StyledImg = styled.img`
+	height: 45px;
+`
 
 export default props =>
 	<StyledHeader>
 		<Row type="flex" justify="space-between">
-			<Col span={4}><OpenFile/></Col>
-			<Col span={16}>
+			<Col span={5}><StyledImg src={Logo}/></Col>
+			<Col span={15}>
 				<Menu intl={props.intl} mode="horizontal"/>
 			</Col>
 			<Col span={2}><LanguageSelector/></Col>
